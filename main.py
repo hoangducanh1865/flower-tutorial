2e92a7b79bc43a2b5a92832af5368cda06c94d3d
hoangducanh1865 <hoangducanh.1865@gmail.com>
2025-07-25 11:19:41 +0700

    “Update”

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
-# from src.flwr_client import client
-# from src.flwr_server import server
-# from src.config import NUM_PARTITIONS, DEVICE
-# from flwr.simulation import run_simulation
+from src.flwr_client import client
+from src.flwr_server import server
+from src.config import NUM_PARTITIONS, DEVICE
+from flwr.simulation import run_simulation
 
-# backend_config = {"client_resources": {"num_cpus": 1, "num_gpus": 1.0}} if DEVICE == "cuda" else {"client_resources": None}
+backend_config = {"client_resources": {"num_cpus": 1, "num_gpus": 1.0}} if DEVICE == "cuda" else {"client_resources": None}
 
-# run_simulation(
-#     server_app=server,
-#     client_app=client,
-#     num_supernodes=NUM_PARTITIONS,
-#     backend_config=backend_config,
-# )
-
-
-# main.py
-
-import flwr as fl
-from src.flwr_client import client_fn
-import sys
-import os
-
-# Fix import path in Kaggle if needed
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
-
-if __name__ == "__main__":
-    fl.simulation.start_simulation(
-        client_fn=client_fn,
-        num_clients=10,
-        config=fl.server.ServerConfig(num_rounds=3),
-    )
+run_simulation(
+    server_app=server,
+    client_app=client,
+    num_supernodes=NUM_PARTITIONS,
+    backend_config=backend_config,
+)
